a89c7ac6e4b9835b3aa465d89ea9130fce30ef4d
Naiara Pacheco <naiarapachecosantos@gmail.com>
2024-04-18 16:15:08 -0300

    update

--- puffy/core/forms.py
+++ puffy/core/forms.py
@@ -7,3 +7,24 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs=
+    {
+        'placeholder': 'Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs=
+    {
+        'placeholder': 'Your e-mail adress',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs=
+    {
+        'placeholder': 'Your Password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=
+    {
+        'placeholder': 'Repeat Password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
