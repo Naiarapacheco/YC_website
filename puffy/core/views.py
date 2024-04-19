d48e27ff18ab9060f407774a4aa7ae6d3ecf6ac3
Naiara Pacheco <naiarapachecosantos@gmail.com>
2024-04-19 10:19:22 -0300

    update

--- puffy/core/views.py
+++ puffy/core/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from item.models import Categoria, Item
-
 from .forms import SignupForm
+
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -17,8 +16,16 @@ def contato(request):
     return render (request, 'core/contato.html')
 
 def signup(request):
-    form = SignupForm()
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login/')
+    else:
+        form = SignupForm()
     
     return render(request, 'core/signup.html', {
-        'form': form
-    })
+       'form':form
+       })
